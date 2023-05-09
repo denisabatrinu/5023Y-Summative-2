c544cd1b1ca49f203fa81b6a34725d5e81b56862
denisabatrinu <rwn21cru@uea.ac.uk>
2023-05-09 20:54:24 +0100

    update script

--- Scripts/plot_code.R
+++ Scripts/plot_code.R
@@ -1,63 +1,76 @@
 #___________________________----
-# SET UP ---
-# Studying the effect of the gene raga-1 and how it interacts with stress on longevity and reproduction in the nematode worm (C. elegans) through gene knockout
-
-
-#__________________________----
 # 📦 PACKAGES ----
-library(tidyverse) # creates data visualisation
-library(readxl) # reads xlsx or xls files
-library(ggpubr) # aids ggplot2
-library(performance) # provides a collection of tools for evaluating and visualizing the performance of predictive models
+library(tidyverse) 
+library(readxl) 
+library(ggpubr) 
+library(GGally)
+library(performance) 
+library(patchwork)
+library(rstatix)
+library(kableExtra)
 
 
-#__________________________----
+#___________________________----
 # 📂 IMPORT DATA ----
-f0_data <- read_excel("Data/elegans_offspring.xlsx", sheet = 1
-                      , col_names = TRUE, col_types = NULL, na = "", skip = 0)
-f1_data <- read_excel("Data/elegans_offspring.xlsx", sheet = 2
-                      , col_names = TRUE, col_types = NULL, na = "", skip = 0)
+# Load the sheets via a read_excel path
+f0_data <- read_excel(path = "Data/elegans_offspring.xlsx", sheet = 1, col_names = TRUE, col_types = NULL)
+f1_data <- read_excel(path = "Data/elegans_offspring.xlsx", sheet = 2, col_names = TRUE, col_types = NULL)
 
 
 #__________________________----
 # 🧹 TIDY ----
-# Convert the "offspring" column to numeric
+# Check columns
+str(f0_data)
+str(f1_data)
+
+# Fix column names so it fits the same format
 f0_data$offspring <- as.numeric(f0_data$offspring)
 f1_data$offsprings <- as.numeric(f1_data$offsprings)
+colnames(f1_data)[4] <- "offspring"
 
-# Remove columns that will not be used
-f0_data <- select(f0_data, -replicate)
-f1_data <- select(f1_data, -parental_treatment)
-
-# Fix column names
-colnames(f1_data)[3] <- "offspring"
-colnames(f1_data)[1] <- "rnai"
-colnames(f1_data)[2] <- "Light_Exposure"
-colnames(f0_data)[2] <- "Light_Exposure"
-
-# Remove NA rows
+# NAs introduced by coercion so will remove NA rows
 f0_data <- na.omit(f0_data)
 f1_data <- na.omit(f1_data)
+
+# Use GGally to check whole plots of the datasets
+GGally::ggpairs(f0_data)
+GGally::ggpairs(f1_data)
+
+# Isolate the rnai and offspring for f0
+f0_data_summary <- f0_data %>%
+  group_by(rnai) %>%
+  summarise(mean = mean(offspring),
+            sd = sd(offspring))
+
+# Isolate the rnai and offspring for f1
+f1_data_summary <- f1_data %>%
+  group_by(parental_rnai) %>%
+  summarise(mean = mean(offspring),
+            sd = sd(offspring))
 
 # Add generation column to each data frame
 f0_data$generation <- "f0"
 f1_data$generation <- "f1"
 
+# Eliminate columns or merge columns to combine dfs later on
+colnames(f1_data)[1] <- "rnai"
+f1_data <- select(f1_data, -parental_treatment)
+f0_data <- select(f0_data, -replicate)
+
 # Combine data frames
 df <- rbind(f0_data, f1_data)
 
 # Convert columns to factors with levels
-df$Light_Exposure <- factor(df$Light_Exposure, levels = c("light", "dark"))
+df$treatment <- factor(df$treatment, levels = c("light", "dark"))
 df$rnai <- factor(df$rnai, levels = c("raga", "ev"))
 df$generation <- factor(df$generation, levels = c("f1", "f0"))
 
 
 #__________________________----
 # 📊PLOTS ----
-ggplot(df, aes(x = rnai, y = offspring, fill = Light_Exposure)) +
+ggplot(df, aes(x = rnai, y = offspring, fill = treatment)) +
   geom_boxplot(color = "black") +
   facet_wrap(~ generation, ncol = 2) +
   scale_fill_manual(values = c("white", "gray")) +
   labs(x = "RNAi Treatment of f0", y = "Offspring Count") +
   theme_classic()
-
